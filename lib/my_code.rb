26bf93b17a8355d022aa634dae0a4c0714ad3b7c
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 00:56:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,6 +9,13 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
+    no_change = []
+  source_index = 0
+  while source_index < source_array.length do
+    no_change << source_array[source_index]
+    source_index += 1
+  end
+  negative
 end
 
 def map_to_double(source_array)
