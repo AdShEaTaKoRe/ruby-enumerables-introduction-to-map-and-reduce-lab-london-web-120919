e00153ac8ae4c9419aa71c7c9ce4bb63000002a0
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 00:56:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,10 +15,17 @@ def map_to_no_change(source_array)
     no_change << source_array[source_index]
     source_index += 1
   end
-  negative
+  no_change
 end
 
 def map_to_double(source_array)
+    negative = []
+  source_index = 0
+  while source_index < source_array.length do
+    negative << source_array[source_index] * -1
+    source_index += 1
+  end
+  negative
 end
 
 def map_to_square(source_array)
